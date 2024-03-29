583a9789338313c0fd542ec1018cb64f3bffbc98
Tanvir Ahmed <100764069+tanvir464@users.noreply.github.com>
2024-03-30 01:54:45 +0600

    Delete Conflicts

--- project/src/project/Project.java
+++ project/src/project/Project.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXML.java to edit this template
  */
-package project;
+package Project;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
@@ -18,7 +18,7 @@ public class Project extends Application {
     
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("SalaryDetailsScene.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("UserProfileScene.fxml"));
         
         Scene scene = new Scene(root);
         
