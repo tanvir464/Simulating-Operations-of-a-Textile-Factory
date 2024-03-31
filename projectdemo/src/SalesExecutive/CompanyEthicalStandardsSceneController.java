9916d2572311c52bb375e532c354289414f4690c
Afra997 <157823071+Afra997@users.noreply.github.com>
2024-03-31 22:44:13 +0600

    Sales Communication Scene to Sales Forecasting Scene

---
+++ projectdemo/src/SalesExecutive/CompanyEthicalStandardsSceneController.java
@@ -0,0 +1,90 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
+ */
+package SalesExecutive;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+
+/**
+ * FXML Controller class
+ *
+ * @author Mahdi Hasan
+ */
+public class CompanyEthicalStandardsSceneController implements Initializable {
+
+    @FXML
+    private Button submitContentButton;
+    @FXML
+    private Button viewContentFeedbackButton;
+    @FXML
+    private Button salesCommunicationButton;
+    @FXML
+    private Button viewGuidelinesButton;
+    @FXML
+    private Button generateCouponsButton;
+    @FXML
+    private Button reviewProductsButton;
+    @FXML
+    private Button advertisementsButton;
+    @FXML
+    private Button createSalesForecastsButton;
+    @FXML
+    private Button logOutButton;
+    @FXML
+    private Button goBackButton;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+
+    @FXML
+    private void submitContentButtonOnClick(ActionEvent event) {
+    }
+
+    @FXML
+    private void viewContentFeedbackButtonOnClick(ActionEvent event) {
+    }
+
+    @FXML
+    private void salesCommunicationButtonOnClick(ActionEvent event) {
+    }
+
+    @FXML
+    private void viewGuidelinesButtonOnClick(ActionEvent event) {
+    }
+
+    @FXML
+    private void generateCouponsButtonOnClick(ActionEvent event) {
+    }
+
+    @FXML
+    private void reviewProductsButtonOnClick(ActionEvent event) {
+    }
+
+    @FXML
+    private void advertisementsButtonOnClick(ActionEvent event) {
+    }
+
+    @FXML
+    private void createSalesForecastsButtonOnClick(ActionEvent event) {
+    }
+
+    @FXML
+    private void logOutButtonOnClick(ActionEvent event) {
+    }
+
+    @FXML
+    private void goBackButtonOnClick(ActionEvent event) {
+    }
+    
+}
